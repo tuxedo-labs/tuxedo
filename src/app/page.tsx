ebc91507ee51fefa92f2734857f6c299e22b2a5c
rafia9005 <rafia9005@gmail.com>
2024-09-27 19:38:44 +0700

    initialize login & register

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
-      <h1>hello world</h1>
+      <Button>hello world</Button>
     </>
   );
 }
